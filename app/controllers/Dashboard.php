3d3eee2725dc3233c38acd38ef20524340a7a477
firay24 <72513085+Firay24@users.noreply.github.com>
2021-06-26 12:12:48 +0700

    login dulu bru dashboard

--- app/controllers/Dashboard.php
+++ app/controllers/Dashboard.php
@@ -3,14 +3,14 @@
 class Dashboard extends Controller{
     public function index()
     {
+        $data['judul'] = 'Dashboard';
+        $this->view('templates/header',$data);
+        $this->view('dashboard/index', $data);
+        
         if ( !isset($_POST['username']) || !isset($_POST['password']))
         {
             header("Location: " . BASEURL . "/home");
-        } else 
-        {
-            $data['judul'] = 'Dashboard';
-            $this->view('templates/header',$data);
-            $this->view('dashboard/index');
-        }
+            exit;
+        } 
     }
 }
